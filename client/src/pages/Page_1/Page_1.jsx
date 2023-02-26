e1185e051db900dab5229ecd678ceba55ec890e4
Vitalii <vexavv2022@gmail.com>
2023-02-26 19:44:49 +0200

    reset

--- client/src/pages/Page_1/Page_1.jsx
+++ client/src/pages/Page_1/Page_1.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Page1(props) {
     return (
         <div>
-            <h1>Welcome to Page #1</h1>
+            <h1>Welcome to Page#1</h1>
         </div>
     );
 }
