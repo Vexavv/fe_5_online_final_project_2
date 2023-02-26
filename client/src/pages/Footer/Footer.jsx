4e9d68cea0de1525557a775b5e5820005ed255d3
unknown <kovalenkosv60@gmail.com>
2023-02-26 13:03:51 +0200

    add fa icons

--- client/src/pages/Footer/Footer.jsx
+++ client/src/pages/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaFacebookF,FaTwitter,FaInstagram,FaPinterest,FaYoutube,FaHeadphonesAlt,FaInbox,FaMapMarkerAlt } from "react-icons/fa";
 import './Footer.scss'; 
 
 function Footer(props) {
@@ -6,11 +7,21 @@ function Footer(props) {
         <div className="box">
             <div className="container">
             <div className="row">
-                <div className="column">                    
-                    <img src="https://cdn.shopify.com/s/files/1/0376/9440/6700/files/footer_logo_180x.png?v=1629543119" alt="" srcset="" />
+                <div className="column">
+                    <a href="/">
+                        <img src="https://cdn.shopify.com/s/files/1/0376/9440/6700/files/footer_logo_180x.png?v=1629543119" alt="" srcset="" />
+                    </a>                    
+                    <br />
+                    <div className="social-icons">
+                    <a href="/"><FaFacebookF/></a>                 
+                    <a href="/"><FaTwitter/></a>                 
+                    <a href="/"><FaInstagram/></a>                 
+                    <a href="/"><FaPinterest/></a>                 
+                    <a href="/"><FaYoutube/></a>                 
+                    </div>
                 </div>
                 <div className="column">
-                <p className="head">Information Company</p>
+                <h2 className="head">Information Company</h2>
                 <a href="/account">My Account</a>
                 <a href="/order">Track Your Order</a>
                 <a href="/faqs">FAQs</a>
@@ -20,7 +31,7 @@ function Footer(props) {
                 <a href="/gift">Gift Card Balance</a>
                 </div>
                 <div className="column">
-                <p className="head">More From Rubix</p>
+                <h2 className="head">More From Rubix</h2>
                 <a href="/rubix">About Rubix</a>
                 <a href="/guarantees">Our Guarantees</a>
                 <a href="/terms">Terms and Conditions</a>
@@ -30,42 +41,12 @@ function Footer(props) {
                 <a href="/sitemap"> Sitemap</a>
                 </div>
                 <div className="column">
-                <p className="head">Let's Talk</p>
-                <p>+391 (0)35 2568 4593</p>
-                <u>hello@domain.com</u>
-                <p className="head">Find Us</p>
-                <p>502 New Design Str <br />Melbourne, Australia</p>
+                <h2 className="head">Let's Talk</h2>
+                <p><FaHeadphonesAlt/> +391 (0)35 2568 4593</p>
+                <u><FaInbox/> hello@domain.com</u>
+                <h2 className="head">Find Us</h2>
+                <p>502 New Design Str <br /><FaMapMarkerAlt/> Melbourne, Australia</p>
                 </div>
-                {/* <div className="column">
-                <a href="#">
-                    <i className="fab fa-facebook-f">
-                    <span style={{ marginLeft: "10px" }}>
-                        Facebook
-                    </span>
-                    </i>
-                </a>
-                <a href="#">
-                    <i className="fab fa-instagram">
-                    <span style={{ marginLeft: "10px" }}>
-                        Instagram
-                    </span>
-                    </i>
-                </a>
-                <a href="#">
-                    <i className="fab fa-twitter">
-                    <span style={{ marginLeft: "10px" }}>
-                        Twitter
-                    </span>
-                    </i>
-                </a>
-                <a href="#">
-                    <i className="fab fa-youtube">
-                    <span style={{ marginLeft: "10px" }}>
-                        Youtube
-                    </span>
-                    </i>
-                </a>
-                </div> */}
             </div>
             </div>
       </div>
