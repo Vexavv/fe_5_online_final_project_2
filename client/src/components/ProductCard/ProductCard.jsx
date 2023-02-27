388a5e6a0cd734b22e3a11a60b063d3ce771049b
Vitalii <vexavv2022@gmail.com>
2023-02-27 18:23:44 +0200

    added display list products

--- client/src/components/ProductCard/ProductCard.jsx
+++ client/src/components/ProductCard/ProductCard.jsx
@@ -3,12 +3,13 @@ import styles from './ProductCard.module.scss'
 import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
 import SearchIcon from '@mui/icons-material/Search';
 import {useSelector} from "react-redux";
+import Button from '../Button/Button'
 
-function ProductCard({name, imageUrls, currentPrice}) {
+function ProductCard({name, imageUrls, currentPrice, myCustomParam}) {
     const display = useSelector(state => state.products.display)
     return (
         <>
-            {display ? ( <li className={styles.Card }>
+            {display ? (<li className={styles.Card}>
                 <img className={styles.CardImg} src={imageUrls[0]} alt="image"/>
                 <div className={styles.CardButton}>
                     <ShoppingBagIcon className={styles.CardButtonIcon}
@@ -25,26 +26,27 @@ function ProductCard({name, imageUrls, currentPrice}) {
                 </div>
                 <div className={styles.CardDescription}>
                     <h5 className={styles.CardDescriptionName}>{name}</h5>
-                    <span className={styles.CardDescriptionPrice}>${currentPrice}.00</span>
+                    <div className={styles.CardDescriptionContainer}>
+                        <span className={styles.CardDescriptionContainerPrice}>${currentPrice}.00</span>
+                        <ul className={styles.CardDescriptionContainerLabel}>{imageUrls.map((item, index) => {
+                            return <li key={index}><img src={item} alt="img"/></li>
+                        })}</ul>
+                    </div>
+
                 </div>
-            </li>) : ( <li className={styles.Row }>
+            </li>) : (<li className={styles.Row}>
                 <img className={styles.RowImg} src={imageUrls[0]} alt="image"/>
-                <div className={styles.CardButton}>
-                    <ShoppingBagIcon className={styles.CardButtonIcon}
-                                     sx={{
-                                         width: 35,
-                                         height: 35,
-                                         padding: 1,
-                                         background: '#ffffff',
-                                         borderRadius: 2,
-                                         marginBottom: 1
-                                     }}/>
-                    <SearchIcon className={styles.CardButtonIcon}
-                                sx={{width: 35, height: 35, padding: 1, background: '#ffffff', borderRadius: 2}}/>
+                <div className={styles.RowDescription}>
+                    <h5 className={styles.RowDescriptionName}>{name}</h5>
+                    <span className={styles.RowDescriptionPrice}>${currentPrice}.00</span>
+                    <ul className={styles.RowDescriptionLabel}>{imageUrls.map((item, index) => {
+                        return <li key={index}><img src={item} alt="img"/></li>
+                    })}</ul>
+                    <span className={styles.RowDescriptionParam}>{myCustomParam}</span>
                 </div>
-                <div className={styles.CardDescription}>
-                    <h5 className={styles.CardDescriptionName}>{name}</h5>
-                    <span className={styles.CardDescriptionPrice}>${currentPrice}.00</span>
+                <div className={styles.RowNav}>
+                    <Button className={styles.RowNavButton} text='Select Options'/>
+                    <Button className={styles.RowNavButton} text='Quick View'/>
                 </div>
             </li>)}
 
