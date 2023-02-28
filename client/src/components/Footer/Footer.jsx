ab5148ca42eb564853bbd5906740b965c82b0992
Svitlana Kovalenko <kovalenkosv60@gmail.com>
2023-02-28 14:26:35 +0200

    adaptive in footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,8 +1,38 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { FaFacebookF,FaTwitter,FaInstagram,FaPinterest,FaYoutube,FaHeadphonesAlt,FaInbox,FaMapMarkerAlt } from "react-icons/fa";
 import styles from './Footer.module.scss'
 
 function Footer(props) {
+    const [isOpen1, setIsOpen1] = useState(false);
+    const [isOpen2, setIsOpen2] = useState(false);
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    function handleResize() {
+      const newWindowWidth = window.innerWidth;
+      setWindowWidth(newWindowWidth);
+      if (newWindowWidth < 990) {
+        setIsOpen2(false);
+        setIsOpen1(false);
+      } else {
+        setIsOpen2(true);
+        setIsOpen1(true);
+      }
+    }
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+    const toggleMenu1 = () => {
+      setIsOpen1(!isOpen1);
+    };
+
+    const toggleMenu2 = () => {
+        setIsOpen2(!isOpen2);
+      };
+
     return (
         <div className={styles.box}>
             <div className={styles.container}>
@@ -21,8 +51,10 @@ function Footer(props) {
                     </div>
                 </div>
                 <div className={styles.column}>
-                <h2 className={styles.head}>Information Company</h2>
-                    <div className={styles.column}>
+                <h2 className={styles.head}>Information Company
+                <button className={styles.burger} onClick={toggleMenu1}>&#8595;</button>
+                </h2>
+                    { isOpen1 && (<div className={styles.wrapper}>
                         <a href="/account">My Account</a>
                         <a href="/order">Track Your Order</a>
                         <a href="/faqs">FAQs</a>
@@ -30,11 +62,13 @@ function Footer(props) {
                         <a href="/shipping">Shipping Guide</a>
                         <a href="/support">Products Support</a>
                         <a href="/gift">Gift Card Balance</a>
-                    </div>
+                    </div>)}
                 </div>
                 <div className={styles.column}>
-                <h2 className={styles.head}>More From Rubix</h2>
-                    <div className={styles.column}>
+                <h2 className={styles.head}>More From Rubix
+                <button className={styles.burger} onClick={toggleMenu2}>&#8595;</button>
+                </h2>
+                    {isOpen2 && (<div className={styles.wrapper}>
                         <a href="/rubix">About Rubix</a>
                         <a href="/guarantees">Our Guarantees</a>
                         <a href="/terms">Terms and Conditions</a>
@@ -42,7 +76,7 @@ function Footer(props) {
                         <a href="/return">Return Policy</a>
                         <a href="/delivery">Delivery & Return</a>
                         <a href="/sitemap"> Sitemap</a> 
-                    </div>
+                    </div>)}
                 </div>
                 <div className={styles.column}>
                 <h2 className={styles.head}>Let's Talk</h2>
@@ -51,7 +85,7 @@ function Footer(props) {
                 <h2 className={styles.head}>Find Us</h2>
                 <p>502 New Design Str <br /><FaMapMarkerAlt/> Melbourne, Australia</p>
                 </div>
-            </div>
+             </div>
             </div>
       </div>
     );
