f0d4afcb04831c95302e3c06d577de6a61920570
Vitalii <vexavv2022@gmail.com>
2023-03-01 14:37:09 +0200

    added product check box components

--- client/src/components/ProductsComponents/ProductsFilter/ProductFilter.jsx
+++ client/src/components/ProductsComponents/ProductsFilter/ProductFilter.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from './ProductFilter.module.scss'
-import classNames from "classnames";
-import RadioButton from "../../RadioButton/RadioButton";
-import RadioButtonColor from "../../RadioButtonColor/RadioButtonColor";
+import RadioButton from "../RadioButton/RadioButton";
+import RadioButtonColor from "../RadioButtonColor/RadioButtonColor";
+import ProductCheckBox from "../ProductCheckBox/ProductCheckBox";
 
 function ProductFilter(props) {
     return (
+
         <div className={styles.Filter}>
             <div className={styles.FilterRadio}>
                 <RadioButton/>
@@ -14,8 +15,9 @@ function ProductFilter(props) {
             <div className={styles.FilterColor}>
                 <RadioButtonColor/>
             </div>
-            <h3 className={classNames(styles.FilterTitle, styles.FilterCategory)}>Price</h3>
-
+            <div className={styles.FilterColor}>
+                <ProductCheckBox/>
+            </div>
         </div>
     );
 }
