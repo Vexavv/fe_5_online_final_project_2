62929ff6d7e98cb62307c1b51f5357a35db7f849
Vitalii <vexavv2022@gmail.com>
2023-03-01 12:34:01 +0200

    added radio button color component

--- client/src/components/ProductsComponents/ProductsFilter/ProductFilter.jsx
+++ client/src/components/ProductsComponents/ProductsFilter/ProductFilter.jsx
@@ -2,22 +2,18 @@ import React from 'react';
 import styles from './ProductFilter.module.scss'
 import classNames from "classnames";
 import RadioButton from "../../RadioButton/RadioButton";
+import RadioButtonColor from "../../RadioButtonColor/RadioButtonColor";
 
 function ProductFilter(props) {
     return (
         <div className={styles.Filter}>
-            <h3 className={styles.FilterTitle}>Product Categories</h3>
             <div className={styles.FilterRadio}>
                 <RadioButton/>
             </div>
-            <h3 className={classNames(styles.FilterTitle, styles.FilterCategory)}>Color</h3>
-            <ul className={styles.FilterColor}>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-            </ul>
+
+            <div className={styles.FilterColor}>
+                <RadioButtonColor/>
+            </div>
             <h3 className={classNames(styles.FilterTitle, styles.FilterCategory)}>Price</h3>
 
         </div>
--- client/src/components/RadioButton/RadioButton.jsx
+++ client/src/components/RadioButton/RadioButton.jsx
@@ -1,10 +1,12 @@
-import React,{useState} from 'react';
+import React, {useState} from 'react';
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
+import FormLabel from '@mui/material/FormLabel';
 import FormControl from '@mui/material/FormControl';
 import RadioButtonUncheckedIcon from "@mui/icons-material/RadioButtonUnchecked";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+
 function RadioButton(props) {
     const [value, setValue] = useState('all');
     const handleChange = (event) => {
@@ -12,22 +14,31 @@ function RadioButton(props) {
     };
     return (
         <FormControl>
+            <FormLabel sx={{color: "#1A1A1A", fontSize: 18, fontWeight: 700, lineHeight: 2,}}
+                       focused={false}
+                       id="demo-radio-buttons-group-label">Product Categories</FormLabel>
             <RadioGroup
                 name="controlled-radio-buttons-group"
                 value={value}
                 onChange={handleChange}>
-                <FormControlLabel value="all" control={<Radio icon={<RadioButtonUncheckedIcon />} checkedIcon={<CheckCircleIcon
-                    sx={{color: 'black'}}/>} />}  label="All Categories" />
-                <FormControlLabel value="furniture" control={<Radio icon={<RadioButtonUncheckedIcon />} checkedIcon={<CheckCircleIcon
-                    sx={{color: 'black'}}/>}/>} label="Furniture" />
-                <FormControlLabel value="chairs" control={<Radio icon={<RadioButtonUncheckedIcon />} checkedIcon={<CheckCircleIcon
-                    sx={{color: 'black'}}/>}/>} label="Chairs" />
-                <FormControlLabel value="sofas" control={<Radio icon={<RadioButtonUncheckedIcon />} checkedIcon={<CheckCircleIcon
-                    sx={{color: 'black'}}/>}/>} label="Sofas" />
-                <FormControlLabel value="lamp" control={<Radio icon={<RadioButtonUncheckedIcon />} checkedIcon={<CheckCircleIcon
-                    sx={{color: 'black'}}/>}/>} label="Lighting Lamp" />
-                <FormControlLabel value="decor" control={<Radio icon={<RadioButtonUncheckedIcon />} checkedIcon={<CheckCircleIcon
-                    sx={{color: 'black'}}/>}/>} label="Decor" />
+                <FormControlLabel value="all"
+                                  control={<Radio icon={<RadioButtonUncheckedIcon/>} checkedIcon={<CheckCircleIcon
+                                      sx={{color: 'black'}}/>}/>} label="All Categories"/>
+                <FormControlLabel value="furniture"
+                                  control={<Radio icon={<RadioButtonUncheckedIcon/>} checkedIcon={<CheckCircleIcon
+                                      sx={{color: 'black'}}/>}/>} label="Furniture"/>
+                <FormControlLabel value="chairs"
+                                  control={<Radio icon={<RadioButtonUncheckedIcon/>} checkedIcon={<CheckCircleIcon
+                                      sx={{color: 'black'}}/>}/>} label="Chairs"/>
+                <FormControlLabel value="sofas"
+                                  control={<Radio icon={<RadioButtonUncheckedIcon/>} checkedIcon={<CheckCircleIcon
+                                      sx={{color: 'black'}}/>}/>} label="Sofas"/>
+                <FormControlLabel value="lamp"
+                                  control={<Radio icon={<RadioButtonUncheckedIcon/>} checkedIcon={<CheckCircleIcon
+                                      sx={{color: 'black'}}/>}/>} label="Lighting Lamp"/>
+                <FormControlLabel value="decor"
+                                  control={<Radio icon={<RadioButtonUncheckedIcon/>} checkedIcon={<CheckCircleIcon
+                                      sx={{color: 'black'}}/>}/>} label="Decor"/>
             </RadioGroup>
         </FormControl>
     );
