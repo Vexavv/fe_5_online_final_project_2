c615bac03f00b35b4e839d61e6095836a31a0315
Vitalii <vexavv2022@gmail.com>
2023-03-06 20:53:17 +0200

    moved the request, loader to App

--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -1,27 +1,12 @@
-import React, {useEffect} from 'react';
-import {useSelector, useDispatch} from "react-redux";
+import React from 'react';
 import styles from './Product.module.scss'
-import {fetchAsyncProducts} from '../../store/productsSlice'
-import Error from '../../components/Error/Error'
-import Loader from '../../components/Loader/Loader'
 import ProductsBanner from "../../components/ProductsComponents/ProductsBaner/ProductsBanner";
 import ProductsNav from "../../components/ProductsComponents/ProductsNav/ProductsNav";
 import ProductsContent from "../../components/ProductsComponents/ProductsContent/ProductsContent";
 import ProductFilter from "../../components/ProductsComponents/ProductsFilter/ProductFilter";
 
 function Products(props) {
-    const dispatch = useDispatch()
-    const products = useSelector(state => state.products.products)
-    const status = useSelector(state => state.products.status)
 
-    useEffect(() => {
-        dispatch(fetchAsyncProducts())
-    }, [dispatch])
-
-    switch (status) {
-        case 'loading':
-            return <Loader/>;
-        case 'loaded':
             return (
                 <main className={styles.Product}>
                     <section>
@@ -34,16 +19,12 @@ function Products(props) {
                             </div>
                             <div className={styles.ProductWrapperContent}>
                                 <ProductsNav/>
-                                <ProductsContent products={products}/>
+                                <ProductsContent />
                             </div>
                         </div>
                     </section>
                 </main>
             );
-        default:
-            return <Error error={status}/>
-    }
-
 }
 
 export default Products;
