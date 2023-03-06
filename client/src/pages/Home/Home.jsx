7d55d1bff659c34ab4331493105601a4f9453569
Татьяна Яцкова <t.yatskova@gmail.com>
2023-03-06 17:23:32 +0200

    add formix

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import SubscriptionNews from "../../components/SubscriptionNews/SubscriptionNews";
+import BigCarousel from "../../components/BigCarousel/BigCarousel"
 import ShopCollection from '../../components/ShopCollection/ShopCollection';
 import Bestsellers from '../../components/Bestsellers/Bestsellers';
-
+import SubscriptionNews from "../../components/SubscriptionNews/SubscriptionNews";
 
 function Home(props) {
-
     return (
         <main>
             <Link to='/product'><ShopCollection/></Link>
+            <BigCarousel/>
             <Bestsellers/>
             <SubscriptionNews/>
+
         </main>
     );
 }
+
+
+
+
+
+
+
+
 
 export default Home;
--- client/src/pages/LoginPage/Login.jsx
+++ client/src/pages/LoginPage/Login.jsx
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {Formik, Form, Field, ErrorMessage} from 'formik';
+import * as yup from 'yup';
+import Button from '../../components/Button/Button'
 
-function Login(props) {
+const loginLink = "true"
+function Login(loginLink) {
+
+ const [isLoginPage, setisLoginPage] = useState("true")
+
+    const initialValues= {
+        firstName:'',
+        lastName:'',
+        email:'',
+        password:''
+       }
+    const validationSchema = yup.object().shape({
+        firstName: yup.string().required('this field is required'),
+        lastName: yup.string().required('this field is required'),
+        email: yup.string().email('the address is filled in with an error').required('this field is required'),
+        password: yup.string().password()
+    })  
+   
     return (
-        <div>
-            <h1> Welcome to Login Page</h1>
+        
+        <div className="loginPage">
+
+           <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+           onSubmit={()=>{}}
+           >
+            {(props)=>(
+            <Form action="">
+                <Field type="text" name ="firstName" placeholder="First Name"/>
+                <ErrorMessage name ="firstName"/>
+                <Field type="text" name ="lastName" placeholder="Last Name"/>
+                <ErrorMessage name ="lastName"/>
+                <Field type="text" name ="email" placeholder="Email"/>
+                <ErrorMessage name ="email"/>
+                <Field type="password" name ="password" placeholder="Password"/>
+                <ErrorMessage name ="password"/>
+                <Button type="submit" text={isLoginPage ? "Sign in":"Create an account"}/>
+            </Form>
+            )}
+            </Formik>      
+           
         </div>
     );
 }
