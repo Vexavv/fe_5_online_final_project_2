658fb2cf555c85e9cb3b791348717975ebd4acd9
Vitalii <vexavv2022@gmail.com>
2023-03-09 20:31:09 +0200

    changes in small carousel

--- client/src/components/SmallCarousel/SmallCarouselItem.jsx
+++ client/src/components/SmallCarousel/SmallCarouselItem.jsx
@@ -12,52 +12,62 @@ import {Link} from "react-router-dom";
 
 function SmallCarouselItem({imageUrls, name, currentPrice, onClick, _id, product}) {
 
-const dispatch = useDispatch()
+    const dispatch = useDispatch()
 
 
     const [hovered, setHovered] = useState(null);
     return (
-            <Card sx={{width:{xs:"350px", sm: "250px", md:"335px"}, border: "none", boxShadow: 0, margin:"0 auto",cursor:"pointer" }}>
-                <Link to='/product'>
-                    <CardMedia className={styles.Wrapper} onClick={()=>{dispatch(getElement(product._id))}}
-                               sx={{height:{xs: "350px", sm: "250px", md:"335px"},
-                                   padding: 1,
-                                   position: "relative"}}
-                               image={hovered === _id && imageUrls.length > 1
-                                   ? imageUrls[1]
-                                   : imageUrls[0]}
-                               onMouseLeave={() => setHovered(null)}
-                               onMouseEnter={() => setHovered(_id)}
-                               title={name}>
-                        <div className={styles.WrapperIcon}>
-                            <HiOutlineShoppingBag className={styles.WrapperIconBtn} />
-                            <TfiSearch className={styles.WrapperIconBtn} onClick={onClick}/>
-                        </div>
-                    </CardMedia>
-                </Link>
-                <CardContent sx={{padding:"20px 0"}}>
-                    <Typography sx={{
-                       textTransform:"capitalize",
-                        color:"#444444",
-                        fontFamily: "Red Hat Display",
-                        fontSize: "16px",
-                        fontWeight: 700,
-                        paddingBottom: "15px",
-                        "&:hover":{color:"#ba933e", transition: "all .5s ease"}
-                    }}
-                                component="div">
-                        {name}
-                    </Typography>
-                    <Typography sx={{
-                        color:"#666666",
-                        fontFamily: "Red Hat Display",
-                        fontSize: "14px",
-                        fontWeight: 400
-                    }}>
-                        ${currentPrice}.00
-                    </Typography>
-                </CardContent>
-            </Card>
+        <Card sx={{
+            width: {xs: "350px", sm: "250px", md: "335px"},
+            border: "none",
+            boxShadow: 0,
+            margin: "0 auto",
+            cursor: "pointer"
+        }}>
+
+            <CardMedia className={styles.Wrapper}
+                       sx={{
+                           height: {xs: "350px", sm: "250px", md: "335px"},
+                           padding: 1,
+                           position: "relative"
+                       }}
+                       image={hovered === _id && imageUrls.length > 1
+                           ? imageUrls[1]
+                           : imageUrls[0]}
+                       onMouseLeave={() => setHovered(null)}
+                       onMouseEnter={() => setHovered(_id)}
+                       title={name}>
+                <div className={styles.WrapperIcon}>
+                    <HiOutlineShoppingBag className={styles.WrapperIconBtn}/>
+                    <TfiSearch className={styles.WrapperIconBtn} onClick={onClick}/>
+                </div>
+            </CardMedia>
+
+            <Link to='/product'><CardContent sx={{padding: "20px 0"}}>
+                <Typography
+                    onClick={() => {dispatch(getElement(product._id))}}
+                    sx={{
+                    textTransform: "capitalize",
+                    color: "#444444",
+                    fontFamily: "Red Hat Display",
+                    fontSize: "16px",
+                    fontWeight: 700,
+                    paddingBottom: "15px",
+                    "&:hover": {color: "#ba933e", transition: "all .5s ease"}
+                }}
+                            component="div">
+                    {name}
+                </Typography>
+                <Typography sx={{
+                    color: "#666666",
+                    fontFamily: "Red Hat Display",
+                    fontSize: "14px",
+                    fontWeight: 400
+                }}>
+                    ${currentPrice}.00
+                </Typography>
+            </CardContent></Link>
+        </Card>
 
     );
 }
