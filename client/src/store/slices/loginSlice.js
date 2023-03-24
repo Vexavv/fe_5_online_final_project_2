086d1a78d0cc7efc041c11d906a9fc372799000b
Vitalii <107429945+Vexavv@users.noreply.github.com>
2023-03-24 20:21:51 +0200

    Revert "Login banch"

--- client/src/store/slices/loginSlice.js
+++ client/src/store/slices/loginSlice.js
@@ -1,119 +1,104 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk  } from '@reduxjs/toolkit'
+import {autorisation} from './localStorageSlice'
 
-// registration fetch
-export const createAccountFetch = createAsyncThunk(
-  'login/createAccountFetch',
-  async ({ firstName, lastName, email, password }, { rejectWithValue, dispatch }) => {
-
-    try {
-      const response = await fetch("http://localhost:3001/api/customers", {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          firstName,
-          lastName,
-          email,
-          password,
-          login: firstName
-        }),
-      })
-
-      const customer = await response.json()
-      if(response.ok) {
-        dispatch(
-          loginCustomerFetch({ email, password })
-        )
-      }
-      else{
-        throw new Error('Could not get data for login')
-      }
-
-      return customer
-    }
-    catch (error) {
-      return rejectWithValue(error.message)
-    }
-  })
-
-// login fetch
-export const loginCustomerFetch = createAsyncThunk(
-  'login/loginCustomerFetch ',
-  async ({ email, password }, { rejectWithValue }) => {
-
-    try {
-      const loggedIn = await fetch("http://localhost:3001/api/customers/login",
-        {
+//  fetches
+export const createAccountFetch = createAsyncThunk (
+    'login/createAccountFetch',
+    async (value, { rejectWithValue, dispatch })=> {
+      try {
+        const respons = await fetch("http://localhost:3001/api/customers", {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
           },
           body: JSON.stringify({
-            loginOrEmail: email,
-            password,
-          })
+            firstName: value.firstName,
+            lastName: value.lastName,
+            email: value.email,
+            password: value.password,
+            login: value.firstName           
+          }),        
         })
 
-      const loggedCustomer = await loggedIn.json()
-      
-      return loggedCustomer
+        const newCustomer = await respons.json()
+       
+        dispatch(
+            loginCustomerFetch({ loginOrEmail: newCustomer.email, password: newCustomer.password })
+        )        
+        return newCustomer
+    } catch (error) {
+        return rejectWithValue(error.message)
+      }
+    })
+    
+
+export const loginCustomerFetch = createAsyncThunk (
+    'login/loginCustomerFetch ',
+    async  (value, {rejectWithValue}, dispatch)=>{
+        try{
+          const loggedIn = await fetch('http://localhost:3001/api/customers/login',
+             {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                  },
+                body: JSON.stringify({
+                  loginOrEmail: value.email,
+                  password: value.password,
+                })              
+                
+              })
+              const  loggedCustomer = await loggedIn.json()
+              // console.log(loggedCustomer.token);
+              // dispatch(autorisation(loggedCustomer.token))
+              return  loggedCustomer
+        }catch(error){
+            return rejectWithValue(error.message)
+        }
     }
-    catch (error) {
-      return rejectWithValue(error.message)
-    }
-  }
 )
 
 
 // slice function
 
 const loginSlice = createSlice({
-  name: 'login',
-
-  initialState: {
-    isLogged: {
-      success: false,
-      token: ''
+    name: 'login',
+    
+    initialState:{
+        isLogged: false,
+        customer: {
+          success:false,
+          token:''
+        }      
     },
-  },
-
-  reducers: {
-    loguotCustomer: (state, action) => {     
-      state.isLogged.token = ''
-      state.isLogged.success = false
+    reducers:{
+       loguotCustomer: (state, action) => {
+        state.isLogged = false
+        state.customer.token = ''
+        state.customer.success = false
+       }
+    },
+     extraReducers: builder => {
+      builder
+      .addCase( createAccountFetch.fulfilled, (state, action) => {
+        state.isLogged = false
+      })     
+      .addCase (createAccountFetch.rejected, (state, action) => {
+        state.isLogged = false
+      })
+      .addCase(loginCustomerFetch.fulfilled, (state, action) => {
+        state.isLogged = true
+        state.customer.token = action.payload.token
+        state.customer.success = action.payload.success
+      })
+      .addCase(loginCustomerFetch.rejected, (state, action) => {
+        state.isLogged = false
+        state.customer = null
+      })      
     }
-  },
-
-  extraReducers: builder => {
-    builder
-      .addCase(createAccountFetch.pending, (state, action) => {        
-        state.isLogged.token = null
-        state.isLogged.success = false
-      })
-      .addCase(createAccountFetch.fulfilled, (state, action) => {       
-        state.isLogged.token = null
-        state.isLogged.success = false
-      })
-      .addCase(createAccountFetch.rejected, (state, action) => {        
-        state.token = false
-        state.success = false
-      })
-      .addCase(loginCustomerFetch.pending, (state, action) => {       
-        state.isLogged.token = false
-        state.isLogged.success = false
-      })
-      .addCase(loginCustomerFetch.fulfilled, (state, action) => {        
-        state.isLogged.token = action.payload.token
-        state.isLogged.success = action.payload.success
-      })
-      .addCase(loginCustomerFetch.rejected, (state, action) => {        
-        state.isLogged.token = null
-        state.isLogged.success = false
-      })
-  }
+    
 })
 
-export const { loguotCustomer } = loginSlice.actions
+export const{loguotCustomer}= loginSlice.actions
 
 export default loginSlice.reducer
