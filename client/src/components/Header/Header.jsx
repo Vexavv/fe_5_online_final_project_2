a16cb704ed30671ec33ea33211f331f7cbf13513
Vitalii <vexavv2022@gmail.com>
2023-04-04 22:12:47 +0300

    fin  badge for wishlist

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { HiOutlineShoppingBag } from "react-icons/hi";
-import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
+import React, {useState} from "react";
+import {Link} from "react-router-dom";
+import {HiOutlineShoppingBag} from "react-icons/hi";
+import {AiOutlineHeart} from 'react-icons/ai';
 
 // import components
 
@@ -14,239 +14,245 @@ import DialogModal from "../DialogModal/DialogModal";
 //import from materialUI
 
 import {
-  AppBar, Box, Toolbar, ButtonGroup, IconButton, Drawer, Divider, Container, Badge,
+    AppBar, Box, Toolbar, ButtonGroup, IconButton, Drawer, Divider, Container, Badge,
 } from "@mui/material";
-
 
 
 // import icons
 import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from "@mui/icons-material/Close";
-import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
-
-import { useSelector } from "react-redux";
+import {useSelector} from "react-redux";
 
 
 // data Menu
 const navItems = [
-  {
-    title: "Home",
-    path: "/",
-  },
-  {
-    title: "Products",
-    path: "/products",
-  },
+    {
+        title: "Home",
+        path: "/",
+    },
+    {
+        title: "Products",
+        path: "/products",
+    },
 
-  {
-    title: "Sale",
-    path: "/sale",
-  },
-  {
-    title: "Contact",
-    path: "/contacts",
-  },
+    {
+        title: "Sale",
+        path: "/sale",
+    },
+    {
+        title: "Contact",
+        path: "/contacts",
+    },
 ];
 
 const Header = (props) => {
-  const cards = useSelector((state) => state.card.products);
+    const cards = useSelector((state) => state.card.products);
+    const {wishlist} = useSelector(state => state.wishlist);
+    const isLogged = useSelector(state => state.isLogged.isLogged.success)
+    let wishlistLength = 0;
+    if (wishlist && wishlist.products) {
+        wishlistLength = wishlist.products.length;
+    }
+    if (!isLogged) {
+        wishlistLength = 0;
+    }
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+    const mobileMenuToggle = () => {
+        setIsMobileMenuOpen((current) => !current);
+    };
 
 
-  const mobileMenuToggle = () => {
-    setIsMobileMenuOpen((current) => !current);
-  };
-
-  return (
-    <>
-      <HeaderBanner />
-      <AppBar
-        position="static"
-        mt={3}
-        sx={{ backgroundColor: "white", boxShadow: "none" }}
-      >
-        <Container maxWidth="xl">
-          <Toolbar
-            disableGutters
-            sx={{
-              justifyContent: "space-between",
-            }}
-          >
-            {/*burger for mobile adaptive */}
-            <Box
-              sx={{
-                flexGrow: { xs: 1, sm: 0 },
-                display: { xs: "flex", sm: "none", md: "none" },
-                flexDirection: "column",
-                alignItems: "center",
-              }}
+    return (
+        <>
+            <HeaderBanner/>
+            <AppBar
+                position="static"
+                mt={3}
+                sx={{backgroundColor: "white", boxShadow: "none"}}
             >
-              <ButtonGroup variant="text" aria-label="text button group">
-                <IconButton
-                  size="large"
-                  aria-label="account of current user"
-                  aria-controls="menu-appbar"
-                  aria-haspopup="true"
-                  onClick={mobileMenuToggle}
-                  color="black"
-                >
-                  {!isMobileMenuOpen && <MenuIcon />}
-                </IconButton>
-                <DialogModal />
+                <Container maxWidth="xl">
+                    <Toolbar
+                        disableGutters
+                        sx={{
+                            justifyContent: "space-between",
+                        }}
+                    >
+                        {/*burger for mobile adaptive */}
+                        <Box
+                            sx={{
+                                flexGrow: {xs: 1, sm: 0},
+                                display: {xs: "flex", sm: "none", md: "none"},
+                                flexDirection: "column",
+                                alignItems: "center",
+                            }}
+                        >
+                            <ButtonGroup variant="text" aria-label="text button group">
+                                <IconButton
+                                    size="large"
+                                    aria-label="account of current user"
+                                    aria-controls="menu-appbar"
+                                    aria-haspopup="true"
+                                    onClick={mobileMenuToggle}
+                                    color="black"
+                                >
+                                    {!isMobileMenuOpen && <MenuIcon/>}
+                                </IconButton>
+                                <DialogModal/>
 
-              </ButtonGroup>
-            </Box>
+                            </ButtonGroup>
+                        </Box>
 
-            {/*burger for tabs adaptive */}
-            <Box
-              sx={{
-                flexGrow: { xs: 1, sm: 1 },
-                display: { xs: "none", sm: "flex", md: "none" },
-                flexDirection: "column",
-                alignItems: "center",
-              }}
-            >
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={mobileMenuToggle}
-                color="black"
-              >
-                {!isMobileMenuOpen && <MenuIcon />}
-              </IconButton>
-            </Box>
+                        {/*burger for tabs adaptive */}
+                        <Box
+                            sx={{
+                                flexGrow: {xs: 1, sm: 1},
+                                display: {xs: "none", sm: "flex", md: "none"},
+                                flexDirection: "column",
+                                alignItems: "center",
+                            }}
+                        >
+                            <IconButton
+                                size="large"
+                                aria-label="account of current user"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
+                                onClick={mobileMenuToggle}
+                                color="black"
+                            >
+                                {!isMobileMenuOpen && <MenuIcon/>}
+                            </IconButton>
+                        </Box>
 
-            {/* drop navigation for mobile */}
-            <Drawer
-              anchor="left"
-              component="div"
-              variant="temporary"
-              open={isMobileMenuOpen}
-              onClose={mobileMenuToggle}
-              ModalProps={{ keepMounted: true }}
-              SlideProps={{ timeout: 400 }}
-              sx={{
-                "& .MuiDrawer-paper": {
-                  boxSizing: "border-box",
-                  width: { xs: "70%", sm: "50%", md: "none" },
-                  overflow: "hidden",
-                  color: "#1A1A1A",
-                  "&:active": { color: "#BA933E" },
-                },
-              }}
-            >
-              <Box
-                sx={{
-                  p: 2,
-                  height: 1,
-                  backgroundColor: "white",
-                }}
-              >
-                <IconButton
-                  size="large"
-                  aria-label="account of current user"
-                  aria-controls="menu-appbar"
-                  aria-haspopup="true"
-                  onClick={mobileMenuToggle}
-                  color="black"
-                  sx={{ mb: 2 }}
-                >
-                  <CloseIcon />
-                </IconButton>
+                        {/* drop navigation for mobile */}
+                        <Drawer
+                            anchor="left"
+                            component="div"
+                            variant="temporary"
+                            open={isMobileMenuOpen}
+                            onClose={mobileMenuToggle}
+                            ModalProps={{keepMounted: true}}
+                            SlideProps={{timeout: 400}}
+                            sx={{
+                                "& .MuiDrawer-paper": {
+                                    boxSizing: "border-box",
+                                    width: {xs: "70%", sm: "50%", md: "none"},
+                                    overflow: "hidden",
+                                    color: "#1A1A1A",
+                                    "&:active": {color: "#BA933E"},
+                                },
+                            }}
+                        >
+                            <Box
+                                sx={{
+                                    p: 2,
+                                    height: 1,
+                                    backgroundColor: "white",
+                                }}
+                            >
+                                <IconButton
+                                    size="large"
+                                    aria-label="account of current user"
+                                    aria-controls="menu-appbar"
+                                    aria-haspopup="true"
+                                    onClick={mobileMenuToggle}
+                                    color="black"
+                                    sx={{mb: 2}}
+                                >
+                                    <CloseIcon/>
+                                </IconButton>
 
-                <Divider sx={{ mb: 2 }} />
-                <Navigation
-                  onClick={() => {
-                    mobileMenuToggle();
-                  }}
-                  navItems={navItems}
-                />
-              </Box>
-            </Drawer>
+                                <Divider sx={{mb: 2}}/>
+                                <Navigation
+                                    onClick={() => {
+                                        mobileMenuToggle();
+                                    }}
+                                    navItems={navItems}
+                                />
+                            </Box>
+                        </Drawer>
 
-            {/* main logo */}
-            <Logo />
+                        {/* main logo */}
+                        <Logo/>
 
-            {/* navbar for desktop */}
-            <Box
-              sx={{
-                flexGrow: 1,
-                display: { xs: "none", sm: "none", md: "table" },
-                textAlign: "center",
-                justifyContent: "space-evenly",
-                alignItems: "center",
-              }}
-            >
-              <Navigation navItems={navItems} />
-            </Box>
+                        {/* navbar for desktop */}
+                        <Box
+                            sx={{
+                                flexGrow: 1,
+                                display: {xs: "none", sm: "none", md: "table"},
+                                textAlign: "center",
+                                justifyContent: "space-evenly",
+                                alignItems: "center",
+                            }}
+                        >
+                            <Navigation navItems={navItems}/>
+                        </Box>
 
-            {/*icons for tab and desc */}
-            <Box
-              sx={{
-                flexGrow: 1,
-                display: "flex",
-                textAlign: "center",
-                justifyContent: { xs: "right", md: "space-evenly" },
-                alignItems: "center",
-              }}
-            >
-              <Login />
-              <Box sx={{ display: { xs: "none", sm: "flex", md: "flex" } }}>
-                <DialogModal />
-              </Box>
+                        {/*icons for tab and desc */}
+                        <Box
+                            sx={{
+                                flexGrow: 1,
+                                display: "flex",
+                                textAlign: "center",
+                                justifyContent: {xs: "right", md: "space-evenly"},
+                                alignItems: "center",
+                            }}
+                        >
+                            <Login/>
+                            <Box sx={{display: {xs: "none", sm: "flex", md: "flex"}}}>
+                                <DialogModal/>
+                            </Box>
 
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                component={Link}
-                to="/cart"
-                color="black"
-                sx={{
-                  "&:hover": { color: "#BA933E" },
-                }}
-              >
-                <Badge
-                  badgeContent={cards.length}
-                  sx={{
-                    color: "gray",
-                    "&:hover": { color: "#BA933E" },
-                  }}
-                >
-                  <HiOutlineShoppingBag />
-                </Badge>
-              </IconButton>
-              <IconButton
-               size="large"
-               aria-label="account of current user"
-               aria-controls="menu-appbar"
-               aria-haspopup="true"
-               component={Link}
-               to="/favorites"
-               color="black"
-               sx={{
-                 "&:hover": { color: "#BA933E" },
-               }}>
-                  <Badge
-                      badgeContent={cards.length}
-                      sx={{
-                          color: "gray",
-                          "&:hover": { color: "#BA933E" },
-                      }}
-                  >
-                <AiOutlineHeart/>
-                      </Badge>
-              </IconButton>
-            </Box>
-          </Toolbar>
-        </Container>
-      </AppBar>
-    </>
-  );
+                            <IconButton
+                                size="large"
+                                aria-label="account of current user"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
+                                component={Link}
+                                to="/cart"
+                                color="black"
+                                sx={{
+                                    "&:hover": {color: "#BA933E"},
+                                }}
+                            >
+                                <Badge
+                                    badgeContent={cards.length}
+                                    sx={{
+                                        color: "gray",
+                                        "&:hover": {color: "#BA933E"},
+                                    }}
+                                >
+                                    <HiOutlineShoppingBag/>
+                                </Badge>
+                            </IconButton>
+                            <IconButton
+                                size="large"
+                                aria-label="account of current user"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
+                                component={Link}
+                                to="/favorites"
+                                color="black"
+                                sx={{
+                                    "&:hover": {color: "#BA933E"},
+                                }}>
+                                <Badge
+                                    badgeContent={wishlistLength}
+                                    sx={{
+                                        color: "gray",
+                                        "&:hover": {color: "#BA933E"},
+                                    }}
+                                >
+                                    <AiOutlineHeart/>
+                                </Badge>
+                            </IconButton>
+                        </Box>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+        </>
+    );
 };
 
 export default Header;
