a58fc8f54d5e5f80b671f6fa6a622ccccbcc4087
Svitlana Kovalenko <kovalenkosv60@gmail.com>
2023-04-06 12:29:43 +0300

    modified:   commonHelpers/mailSender.js

--- commonHelpers/mailSender.js
+++ commonHelpers/mailSender.js
@@ -2,7 +2,7 @@ const nodemailer = require("nodemailer");
 const keys = require("../config/keys");
 const getConfigs = require("../config/getConfigs");
 
-module.exports = async (subscriberMail, letterSubject, letterHtml, res) => {
+module.exports = async (subscriberMail, letterSubject,   letterHtml, res) => {
   const configs = await getConfigs();
 
   //authorization for sending email
@@ -37,3 +37,4 @@ module.exports = async (subscriberMail, letterSubject, letterHtml, res) => {
 
   return result;
 };
+  
